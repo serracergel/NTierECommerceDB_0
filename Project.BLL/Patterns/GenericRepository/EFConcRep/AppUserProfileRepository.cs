f3cb71de2ec86bee231853c6aac8282fcef64b1f
Dilara GUREL <dilara.gurel@bilgeadam.com>
2023-10-25 12:06:48 +0300

    project.bll katmanında classlar düzenlendi

---
+++ Project.BLL/Patterns/GenericRepository/EFConcRep/AppUserProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Patterns.GenericRepository.EFConcRep
+{
+    public class AppUserProfileRepository
+    {
+    }
+}
---
+++ Project.BLL/Patterns/GenericRepository/EFConcRep/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Project.BLL.Patterns.GenericRepository.MongoBaseRep;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Patterns.GenericRepository.EFConcRep
+{
+    public class CategoryRepository:BaseRepository<Category>
+    {
+    }
+}
